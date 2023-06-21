3b706eef8003cc394d29389265cb5d707b7e3943
Aman <singhaman021202@gmail.com>
2023-06-22 03:38:53 +0530

    commit

--- app/src/main/java/com/example/craftopia_master/searchview.kt
+++ app/src/main/java/com/example/craftopia_master/searchview.kt
@@ -2,6 +2,8 @@ package com.example.craftopia_master
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
@@ -17,6 +19,33 @@ class searchview : AppCompatActivity() {
 
         val drawerLayout: DrawerLayout = findViewById(R.id.drawerLayout)
         val navView: NavigationView = findViewById(R.id.nav_view)
+
+        toggle = ActionBarDrawerToggle(this,drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+       supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+                when(it.itemId){
+                    R.id.user -> Toast.makeText(applicationContext,
+                        "Clicked on User", Toast.LENGTH_LONG).show()
+
+                    R.id.yourOrder -> Toast.makeText(applicationContext,
+                        "To display your order", Toast.LENGTH_LONG).show()
+
+                    R.id.Become_A_Seller -> Toast.makeText(applicationContext,
+                        "Wanted to be a seller", Toast.LENGTH_LONG).show()
+                }
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+         return super.onOptionsItemSelected(item)
     }
 
 
