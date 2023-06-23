e71eb2ac28a28334b86a5c8ce930136ceae39df4
Aman <singhaman021202@gmail.com>
2023-06-23 12:46:50 +0530

    commit

--- app/src/main/java/com/example/craftopia_master/MainActivity.kt
+++ app/src/main/java/com/example/craftopia_master/MainActivity.kt
@@ -1,18 +1,38 @@
 package com.example.craftopia_master
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import com.google.firebase.auth.FirebaseAuthException
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var auth:FirebaseAuthException
-
+ private lateinit var logo: ImageView
+   private lateinit var splash1: ImageView
+   private lateinit var splash2:ImageView
+   private lateinit var splash3:ImageView
+   private lateinit var splash4:ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        logo= findViewById(R.id.imageView)
+        splash1= findViewById(R.id.splash1)
+        splash2= findViewById(R.id.splash2)
+        splash3= findViewById(R.id.splash3)
+        splash4= findViewById(R.id.splash4)
+
+        val animationalLeftToRight:Animation=AnimationUtils.loadAnimation(applicationContext,R.anim.left_to_right)
+        splash1.startAnimation(animationalLeftToRight)
+        splash3.startAnimation(animationalLeftToRight)
+
+        val animationRightToLeft: Animation = AnimationUtils.loadAnimation(applicationContext, R.anim.right_to_left)
+        splash2.startAnimation(animationRightToLeft)
+        splash4.startAnimation(animationRightToLeft)
 
     }
 }
